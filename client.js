72d0bb7c11376b4bead013b56b2c66af78ab33f1
Sszyh <zhuyer@outlook.com>
2022-07-26 18:25:14 -0700

    add name

--- client.js
+++ client.js
@@ -8,7 +8,8 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
   conn.on("connect", () => {
-    console.log("connected!");
+    console.log("Successfully connected to game server");
+    conn.write("Name: SAH");
   })
   conn.on("data", (data) => {
     console.log(data);
