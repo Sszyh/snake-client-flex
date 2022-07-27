2d5f3936a53a817cc909d5f75c73121f9b940036
Sszyh <zhuyer@outlook.com>
2022-07-26 18:17:16 -0700

    use ES6 shorthand sytnax to exports connect function

--- play.js
+++ play.js
@@ -1,23 +1,24 @@
 const net = require("net");
+const { connect } = require("./client");
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "192.168.0.36",
-    port: 50541
-  });
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: "192.168.0.36",
+//     port: 50541
+//   });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    console.log("connected!");
-  })
-  conn.on("data", (data) => {
-    console.log(data);
-  })
-  return conn;
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
+//   conn.on("connect", () => {
+//     console.log("connected!");
+//   })
+//   conn.on("data", (data) => {
+//     console.log(data);
+//   })
+//   return conn;
 
-};
+// };
 
 console.log("Connecting ...");
 connect();
