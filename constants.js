971b4b46dc6ec8cd54727e643d88f1116f7160c0
Sszyh <zhuyer@outlook.com>
2022-07-28 14:05:31 -0700

    add constants.js

---
+++ constants.js
@@ -0,0 +1,17 @@
+const IP = "192.168.0.36";
+const PORT = 50541;
+const playName = "Name: SAH";
+const obj = {
+  w: "Move: up",
+  a: "Move: left",
+  s: "Move: down",
+  d: "Move: right",
+  z: "Say: today is so hot!"
+};
+module.exports = {
+  IP,
+  PORT,
+  playName,
+  obj
+};
+
